b2699be19ad4426a0109f86bc407f43f8f23308d
ohjw26 <zionyy@skuniv.ac.kr>
2023-12-23 22:20:58 +0900

    Fist commit in devC branch

--- MyGit/src/mygit/A.java
+++ MyGit/src/mygit/A.java
@@ -4,5 +4,7 @@ public class A {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 		System.out.println("This is the 2nd version");
+		System.out.println("This is the 3rd version");
+		System.out.println("This is the 4th version");
 	}
 }
